8d4ae43ba2644f51b9e021bdd399d11a50326972
浅陌 <2141954260@qq.com>
2025-08-18 00:35:59 +0800

    更新pei

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [{
     name: "WeChatBot", // 你的应用名称
-    script: "./dist/bundle.js", // 你的项目入口文件，这里指向esbuild打包后的文件
+    script: "./dist/bundle.cjs", // 你的项目入口文件，这里指向esbuild打包后的文件
     // 下面是可选配置
     instances: 1, // 启动的实例数量，'max' 表示所有可用CPU核心
     autorestart: true, // 崩溃后自动重启
